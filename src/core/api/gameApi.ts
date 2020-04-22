9166b1cab87d1f2ed8a1ed6bf6473ced399a28b6
com <you@example.com>
2020-04-23 00:58:11 +0500

    update gameApi req

--- src/core/api/gameApi.ts
+++ src/core/api/gameApi.ts
@@ -1,5 +1,16 @@
+// export const fetchGames = () => {
+//   return fetch(
+//     "https://jsonplaceholder.typicode.com/todos/1"
+//   ).then((response) => response.json());
+// };
+
 export const fetchGames = () => {
-  return fetch(
-    "https://jsonplaceholder.typicode.com/todos/1"
-  ).then((response) => response.json());
+  try {
+    const response = fetch(
+      "https://www.mafiaonline.ru/api/api.php?action=log&param=current"
+    ).then((res) => res.json());
+    return response;
+  } catch (error) {
+    return error;
+  }
 };
